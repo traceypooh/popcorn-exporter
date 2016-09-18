9c756304d6d091ce754a56daf6dab5e21be08828
Laurian Gridinoc <laurian@gridinoc.name>
2016-09-18 11:35:54 +0100

    rabbitmq start, setup

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+/etc/init.d/rabbitmq-server start
+rabbitmqctl add_vhost myvhost
+rabbitmqctl add_user myuser mypassword
+rabbitmqctl set_permissions -p myvhost myuser ".*" ".*" ".*"
 celery -A server.celery worker &
 uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi
